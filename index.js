522e28094c2141602975ea2b0b099b97910560ae
Jack Lach <jack.lach4@gmail.com>
2023-02-12 03:40:14 -0500

    all tests passing after npm version downgrade to 16.14.2

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+
+
+
+const submitData = (nameString, emailString) => {
+    let formData = {
+        name: nameString,
+        email: emailString,
+    };
+
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(res => res.json())
+            .then(object => {
+                console.log(object)
+                let IdNumber = object.id;
+                let h2 = document.createElement('h2');
+                h2.textContent = IdNumber;
+                document.querySelector('body').appendChild(h2);
+            })
+
+                .catch(error => {
+                    alert("An error has occured");
+                    console.log(error.message);
+                    let h3 = document.createElement('h3');
+                    h3.textContent = "request to http://localhost:3000/users failed, reason: Unauthorized Access";
+                    document.querySelector('body').appendChild(h3);
+                });
+};
